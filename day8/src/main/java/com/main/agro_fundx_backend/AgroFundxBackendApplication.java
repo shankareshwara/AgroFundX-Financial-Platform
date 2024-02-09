a91ab49728f915e4bc5954eab2e997f0b7d5cf2f
shankareshwara p <shankareshwara11@gmail.com>
2024-02-09 11:42:24 +0530

    test

---
+++ day8/src/main/java/com/main/agro_fundx_backend/AgroFundxBackendApplication.java
@@ -0,0 +1,13 @@
+package com.main.agro_fundx_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroFundxBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgroFundxBackendApplication.class, args);
+	}
+
+}
---
+++ day9/src/main/java/com/main/agro_fundx_backend/entity/Role.java
@@ -0,0 +1,6 @@
+package com.main.agro_fundx_backend.entity;
+
+public enum Role {
+    USER,
+    ADMIN
+}
